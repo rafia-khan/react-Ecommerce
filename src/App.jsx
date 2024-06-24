062346d67034d6da35e829fa504a2930c2f1a322
rafia-khan <khanrafia9218@gmail.com>
2024-06-24 12:59:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navigation from './components/Navigation';
+
+
+
+function App() {
+
+
+  return (
+      <div className="">
+        <div>
+        <Navigation/>
+        </div>
+      </div>
+    
+  )
+}
+
+export default App;
